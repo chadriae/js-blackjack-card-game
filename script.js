1baa56510f7115e499f707190e143d54140962b2
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2020-11-20 12:31:55 +0100

    Add play again button when won

--- script.js
+++ script.js
@@ -23,14 +23,26 @@ if (currentScore < 21) {
 if (currentScore == 21) {
     document.getElementById("drawnCard").innerHTML = "You drew: " + actualNumber + ".";
     document.getElementById("imageCard").innerHTML = '<img src='+ imagesArray[actualNumber - 1] + '>';
-    document.getElementById("currentScore").innerHTML = `CONGRATS! YOU WON! You got: ${currentScore} points`;
+    document.getElementById("currentScore").innerHTML = `CONGRATS! YOU WON! You got ${currentScore} points`;
 
+    document.getElementById("confirm").style.display = "none";
+
+    let element = document.getElementById("playAgain");
+    element.innerHTML = "PLAY AGAIN";
+    element.classList.add("bg-green-500");
+    element.classList.add("px-8");
+    element.classList.add("py-4");
+    element.classList.add("rounded-lg");
+    element.classList.add("font-bold");
+    element.addEventListener("click", function () {
+        location.reload();
+    })
 }
 
 else if (currentScore > 21) {
     document.getElementById("drawnCard").innerHTML = "You drew: " + actualNumber + ".";
     document.getElementById("imageCard").innerHTML = '<img src='+ imagesArray[actualNumber - 1] + '>';
-    document.getElementById("currentScore").innerHTML = "Too bad, you lost! Your score is: " + currentScore + ". You can play again by pressing the button.";
+    document.getElementById("currentScore").innerHTML = "Too bad, you lost! Your score is " + currentScore + ". You can play again by pressing the button.";
 
     document.getElementById("confirm").style.display = "none";
 
